09193adc31c1280effbd99244625a41b6d88b1cf
harapoke <42713371+harapoke@users.noreply.github.com>
2020-04-26 00:37:15 +0200

    register updates
    - started working on registration 
    - some input errors are already added, they are working, but im going to add some more and make warning text for them
    - logging in is working now after registration

--- register.php
+++ register.php
@@ -2,16 +2,42 @@
 session_start();
 
 require_once('src/templating/ViewGenerator.php');
-//require_once('src/database_handler/DH_user.php');
-//require_once("src/model/user.php");
+require_once('src/database_handler/DH_user.php');
+require_once("src/model/user.php");
 
-//$DH_user = new DH_user();
+if (isset($_POST["sign_up_button"])) {
+	
+	$name = $_POST["user_name"];
+	$birthDate = $_POST["user_birth"];
+	$email = $_POST["user_email"];
+	$password = $_POST["user_password"];
+	$telNum = $_POST["user_tel"];
+	
+	$DH_user = new DH_user();
+	$id = $DH_user->getNewId();
+	
+	$isCorrect = correctionCheck($password, $telNum);
+	
+	if ($isCorrect) {
+		$user = new User($id, $name, $birthDate, $email, $password, $telNum);
+		$DH_user->addUser($user);
+	}
+}
+
+function correctionCheck($password, $telNum) {
+	if(strlen($password) < 6 || !preg_match('/[A-Za-Z-z]/', $password) || !preg_match('/[0-9]/', $password)) {
+		return false;
+	} else if (strlen($telnum) < 0 || strlen($telnum) > 10 || !preg_match('/[0-9]/', $password) || preg_match('/[A-Za-Z-z]/', $password))  {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
 
 //test user
 //$user = new User(0, "aladni", "01/JAN/1990", "ali@jamba.hu", "bumbum", "123456722");
-
-//this function checks if the given informations are correct
-
 
 
 //this adds the user to the file, also for test purposes as this file made
@@ -21,6 +47,4 @@ require_once('src/templating/ViewGenerator.php');
 
 
 $generator = new ViewGenerator();
-
 $generator->generate("register_view", $_SESSION["username"]);
-//echo __DIR__;
