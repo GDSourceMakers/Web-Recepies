3acde1f7f6e2481d5a603215c184097f1a52aaaa
Laura Vas <v.luva99@gmail.com>
2020-04-25 18:22:23 +0200

    login error view
     - added text to appear, when the login data is incorrect

--- views/login_view.php
+++ views/login_view.php
@@ -1,16 +1,7 @@
 <link rel="stylesheet" type="text/css" href="static/css/sign_up.css">
 
 <?php
-	//print_r($data);
-
-	//checking if the login was success + first visit of the page
-	if ($data->visiter == true) {}
-	else if ($data->showBadLogin == true) {
-		echo "bad login";
-	}
-	else if($data->successfulLogin == true){
-		echo "you are logged in";
-	}
+	
 ?>
 
 <!--coped the content of "sign_in.html" here-->
@@ -21,6 +12,19 @@
 		<label for="user_email">E-mail: </label><input id="user_email" type="email" name="username"><br>
 		<label for="user_password">Password: </label><input type="password" id="user_password" name="pwd"><br>
 
+		<?php
+			//checking if the login was success + first visit of the page
+			if ($data->visiter == true) {}
+			else if ($data->showBadLogin == true) {
+				$template = <<<EOT
+				<div style="color:var(--secondary-dark-color);text-align:center;font-weight:600;">Login unsuccessful! Wrong e-mail or password.</div>
+				EOT;
+				echo $template; //echo here
+			}
+			else if($data->successfulLogin == true){
+				echo "you are logged in";
+			}
+		?>
 		<input id="sign_up_button" type="submit" value="Log in" name="login-btn"><br>
 	</form>
 	<div class="goto_sign_up">
