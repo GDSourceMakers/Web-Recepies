ad67555f55401337a95bb602f461e9536b3a466f
harapoke <42713371+harapoke@users.noreply.github.com>
2020-04-26 20:36:46 +0200

    register updates
    - the contact is working now

--- index.php
+++ index.php
@@ -6,6 +6,20 @@ session_start();
 
 require_once('src/templating/ViewGenerator.php');
 
+$feedbackCounter = 0;
+
+if (isset($_POST["contact_btn"])) {
+	
+	
+	$file = fopen("database/feedback/" . "feedback" . $feedbackCounter . ".txt", "w");
+	fwrite($file, $_POST["contact_us"]);
+	fclose($file);
+	
+	$feedbackCounter++;
+	
+}
+
+
 $generator = new ViewGenerator();
 
 $generator->generate("welcome_view", null);
