14d2ae5418cb5d8516fa7db4d41541a89d1962f7
harapoke <42713371+harapoke@users.noreply.github.com>
2020-04-26 20:58:28 +0200

    register fixed

--- register.php
+++ register.php
@@ -1,43 +1,98 @@
 <?php
 session_start();
-//this is the connector to the index_view.php
-
-//readfile('views/template.html');
 
 require_once('src/templating/ViewGenerator.php');
+require_once('src/database_handler/DH_user.php');
+require_once("src/model/user.php");
 
-
-
-if (isset($_POST["contact_btn"])) {
+if (isset($_POST["sign_up_button"])) {
 	
-	$feedbackCounter = 0;
+	$name = $_POST["user_name"];
+	$birthDate = $_POST["user_birth"];
+	$email = $_POST["user_email"];
+	$password = $_POST["user_password"];
+	$passwordAgain = $_POST["user_password_again"];
+	$telNum = $_POST["user_tel"];
 	
-	try{
-            $file = fopen("database/feedback/" . "feedback_index.txt", "r");
-            $feedbackCounter = fgets($file);
-            fclose($file);
-        } catch(Exception $e){
-            
+	$isCorrect = true;
+	
+	$DH_user = new DH_user();
+	
+	
+	$accounts = $DH_user->getAllUsers();
+	$usernameError = false;
+	
+	
+//this checks if the username is already used
+//has to be fixed !!!	
+    foreach ($accounts as $acc) {
+        if ($acc->name ==  $name) {
+            $usernameError = true;
+			break;
         }
-		
+    }
 	
-	$nextfeedback = $feedbackCounter + 1;
+//username checker
+	if($name == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The username is required!";
+	} else if ($usernameError) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The username is already being used! Please select a different one!";
+	}
+//birthdate checker
+	else if($birthDate == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The birthdate is required!";
+	} else if ($birthDate > date('Y-m-d')) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: Sorry, you are too young to use this website!";
+	} else if ($birthDate < "1900-01-01") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: Sorry, but only the living ones shall use this website! :)";
+	}
+//email checker
+	else if($birthDate == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The email is required!";
+	}
+//password checker	
+	else if ($password == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password is required!";
+	} else if (strlen($password) < 5) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password is too short! Please write a longer one!";
+	} else if (!preg_match('/[A-Za-z]/', $password) || !preg_match('/[0-9]/', $password)) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password must contain letters (lower AND uppercase) and numbers!";
+	} else if ($passwordAgain == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The  confirmation password is required!";
+	} else if ($password != $passwordAgain) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password and the confirmation password do not match!";
+	}	
 	
-	$file = fopen("database/feedback/" . "feedback" . $feedbackCounter . ".txt", "w");
-	fwrite($file, $_POST["contact_us"]);
-	fclose($file);
+	if ($isCorrect) {
+		$id = $DH_user->getNewId(); //ID giving here, so failed attempts won't raise the number too
+		$user = new User($id, $name, $birthDate, $email, $password, $telNum);
+		$DH_user->addUser($user);
+	} else {
+		die($error);
+	}
 	
-	$file = fopen("database/feedback/" . "feedback_index.txt", "w");
-        fseek($file,0);
-        fwrite($file, $nextfeedback);
-
-        fclose($file);
-		
 }
 
 
-$generator = new ViewGenerator();
+//test user
+//$user = new User(0, "aladni", "01/JAN/1990", "ali@jamba.hu", "bumbum", "123456722");
 
-$generator->generate("welcome_view", null);
-//echo __DIR__;
-?>
+//this adds the user to the file, also for test purposes as this file made
+//didnt want to manually write down the user's data into the file (+it shows the functions are working)
+//$DH_user->addUser($user);
+
+
+
+$generator = new ViewGenerator();
+$generator->generate("register_view", null); 
