15638901cc25c5f3093155bab0890e27b40f1d34
harapoke <42713371+harapoke@users.noreply.github.com>
2020-04-26 16:52:38 +0200

    register updates
    - it throws error when the required field arent filled
    - it checks if the password and the confirmation passwords are the same
    - other checkers are added, for example the password has to have more than 5 characters
    - it throws error for the username checker
    - also they might be a problem with the getNewId function, it always sets the lastId to 0 so it can add only 1 more users

--- register.php
+++ register.php
@@ -11,28 +11,74 @@ if (isset($_POST["sign_up_button"])) {
 	$birthDate = $_POST["user_birth"];
 	$email = $_POST["user_email"];
 	$password = $_POST["user_password"];
+	$passwordAgain = $_POST["user_password_again"];
 	$telNum = $_POST["user_tel"];
+	
+	$isCorrect = true;
 	
 	$DH_user = new DH_user();
 	$id = $DH_user->getNewId();
 	
-	$isCorrect = correctionCheck($password, $telNum);
+	$accounts = $DH_user->getAllUsers();
+	$usernameError = false;
+	
+//this checks if the username is already used
+//has to be fixed !!!	
+    foreach ($accounts as $acc) {
+        if ($acc->name ==  $name) {
+            $usernameError = true;
+			break;
+        }
+    }
+	
+//username checker
+	if($name == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The username is required!";
+	} else if ($usernameError) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The username is already being used! Please select a different one!";
+	}
+//birthdate checker
+	else if($birthDate == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The birthdate is required!";
+	}
+//email checker
+	else if($birthDate == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The email is required!";
+	}
+//password checker	
+	else if ($password == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password is required!";
+	} else if (strlen($password) < 5) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password is too short! Please write a longer one!";
+	} else if (!preg_match('/[A-Za-z]/', $password) || !preg_match('/[0-9]/', $password)) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password must contain letters (lower AND uppercase) and numbers!";
+	} else if ($passwordAgain == "") {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The  confirmation password is required!";
+	} else if ($password != $passwordAgain) {
+		$isCorrect = false;
+		$error = "<b>ERROR</b>: The password and the confirmation password do not match!";
+	}	
 	
 	if ($isCorrect) {
 		$user = new User($id, $name, $birthDate, $email, $password, $telNum);
 		$DH_user->addUser($user);
+	} else {
+		die($error);
 	}
+	
 }
-
-function correctionCheck($password, $telNum) {
-	return true;
-}
-
 
 
 //test user
 //$user = new User(0, "aladni", "01/JAN/1990", "ali@jamba.hu", "bumbum", "123456722");
-
 
 //this adds the user to the file, also for test purposes as this file made
 //didnt want to manually write down the user's data into the file (+it shows the functions are working)
