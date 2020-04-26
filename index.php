4c2fe928e0299832994cab54a86f02c9394bcf8d
harapoke <42713371+harapoke@users.noreply.github.com>
2020-04-26 20:57:38 +0200

    index updates
    - this time written to the index.php

--- index.php
+++ index.php
@@ -6,17 +6,31 @@ session_start();
 
 require_once('src/templating/ViewGenerator.php');
 
-$feedbackCounter = 0;
+
 
 if (isset($_POST["contact_btn"])) {
 	
+	$feedbackCounter = 0;
+	
+	try{
+            $file = fopen("database/feedback/" . "feedback_index.txt", "r");
+            $feedbackCounter = fgets($file);
+            fclose($file);
+        } catch(Exception $e){
+            
+        }
+	
+	$nextfeedback = $feedbackCounter + 1;
 	
 	$file = fopen("database/feedback/" . "feedback" . $feedbackCounter . ".txt", "w");
 	fwrite($file, $_POST["contact_us"]);
 	fclose($file);
 	
-	$feedbackCounter++;
-	
+	$file = fopen("database/feedback/" . "feedback_index.txt", "w");
+        fseek($file,0);
+        fwrite($file, $nextfeedback);
+
+        fclose($file);
 }
 
 
